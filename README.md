5fc54ea1c185c31614901d8c6ea4404147986ae1
Piotr Krawiec <pkrawiec@pgs-soft.com>
2021-07-20 13:22:51 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # mosaic-trunk-rush-version-report-action
 Creates a version report for a Rush project with a trunk based development approach.
+
+## Inputs
+
+### `report-id`
+
+**Required** Name of the report. Needs to consist of `digits (0-9)`, `letters(A-Z, a-z)`, separated by `-`.
+
+### `stack`
+
+Name of the stack, like dev or prod. Default `dev`.
+
+### `table-name`
+
+Name of the table to hold the reports. Default `rush-version-reports`.
+
+### `repository-name`
+
+Name of the repository
+
+### `repository-version`
+
+Repository version
+
+## Outputs
+
+### `version-details`
+
+Project versions.
+
+## Prerequisites
+- Repository needs to have a `rush.json` file
+- Dynamodb table needs to exist (sample template included in the files)
+- Action needs to have basic write permissions on the table
+
+## Example usage
+
+```
+uses: advancedcsg-open/mosaic-trunk-rush-version-report-action@v1.0
+with:
+    report-id: 'sample'
+    stack: 'dev'
+    repository-name: 'example-repo/project'
+    repository-version: 'v1.0.0'
+```
---
+++ index.js
@@ -0,0 +1,72 @@
+const core = require('@actions/core');
+const AWS = require('aws-sdk')
+const fs = require('fs')
+
+const dynamodb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+
+(async () => {
+    try {
+        const repositoryName = core.getInput('repository-name');
+        const repositoryVersion = core.getInput('repository-version');
+        const reportId = core.getInput('report-id');
+        const tableName = core.getInput('table-name');
+
+        let versionDetails = await processVersions(tableName, reportId, repositoryName, repositoryVersion)
+
+        core.setOutput("version-details", versionDetails);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+})()
+
+
+async function processVersions(tableName, reportId, repositoryName, repositoryVersion) {
+    let rushFile = fs.readFileSync(`rush.json`)
+    let rush = JSON.parse(rushFile)
+    let projectLocations = rush["projects"]
+    let projects = getProjectVersions(projectLocations)
+
+    const date = new Date().toISOString()
+
+    console.info(`Repository version: ${repositoryVersion}`)
+
+    let versionDetails = {
+        'repository': repositoryName,
+        'version': repositoryVersion,
+        'date': date,
+        'projects': projects
+    }
+
+    let dynamodbItem = {...versionDetails};
+    dynamodbItem['PK'] = reportId
+    dynamodbItem['SK'] = `PROJECTS#${repositoryName}#${repositoryVersion}`
+    let params = {
+        TableName: tableName,
+        Item: dynamodbItem
+    };
+
+    dynamodb.put(params, function (err) {
+        if (err) {
+            throw err
+        } else {
+            console.log("Successfully added version");
+        }
+    });
+
+    return versionDetails
+}
+
+function getProjectVersions(projectLocations) {
+    let projects = {}
+    projectLocations.forEach(function (project) {
+        let projectFolder = project['projectFolder']
+        let projectFileLocation = `${projectFolder}/package.json`
+        let projectFile = fs.readFileSync(projectFileLocation)
+        let packageData = JSON.parse(projectFile);
+        let name = packageData['name']
+        let version = packageData['version']
+        projects[name] = version
+        console.info(`Name: ${name}, version: ${version}`)
+    })
+    return projects
+}
